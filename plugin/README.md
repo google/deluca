47820091b9982860b69ef84d32d1fbb45e8a1ca9
Daniel Suo <danielsuo@gmail.com>
2021-04-16 16:15:28 -0400

    Add scripts to generate plugins

---
+++ plugin/README.md
@@ -0,0 +1,15 @@
+# deluca-plugin
+
+Welcome to your new `Deluca Plugin (TM)`!
+
+## IMPORTANT!
+`deluca` uses [Python namespace
+packages](https://packaging.python.org/guides/packaging-namespace-packages/) to
+handle its plugins. In order to maintain this, we recommend you do not create
+the file `[PLUGIN_ROOT]/deluca/__init__.py`.
+
+## Getting started
+1. Optional, but good to fill out `[PLUGIN_ROOT]/setup.cfg` as appropriate.
+2. Start writing
+3. Create a pull request [here](https://github.com/google/deluca/pulls) if you'd
+   like us to review your plugin and add it to the official list.
---
+++ scripts/make_plugin.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+
+import sys
+import argparse
+import pathlib
+import shutil
+import fileinput
+
+parser = argparse.ArgumentParser(
+    description="A script that generates the template for a new plugin"
+)
+parser.add_argument("name", type=str, help="Name of the package, formatted as deluca-name")
+parser.add_argument(
+    "-d",
+    "--dst",
+    type=pathlib.Path,
+    help="Parent directory to place new plugin (default: '.')",
+    default=pathlib.Path("."),
+)
+
+args = parser.parse_args()
+
+
+def err_and_die(message):
+    print(f"{message}\n")
+    parser.print_help()
+    sys.exit(1)
+
+
+if not args.dst.exists() or not args.dst.is_dir():
+    err_and_die(f"ERROR: '{args.dst}' either does not exist or is not a directory!")
+    
+path = args.dst.absolute() / f"deluca-{args.name}"
+
+if path.exists():
+    err_and_die(f"ERROR: '{path}' already exists!")
+
+src = pathlib.Path(__file__).absolute().parent.parent / "plugin"
+
+shutil.copytree(src, path)
+
+for file_path in path.glob("**/*"):
+    if file_path.is_file():
+        with fileinput.FileInput(file_path.absolute(), inplace=True) as file:
+            for line in file:
+                print(line.replace("deluca-plugin", f"deluca-{args.name}"), end="")
+
+lib_path = path / "deluca" / args.name
+lib_path.mkdir()
+
+with open(lib_path / "__init__.py", "w") as init_file:
+    pass
