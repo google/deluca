233e82712bd4aafccf01f1d57d5035b9a99d7e2a
Daniel Suo <danielsuo@gmail.com>
2021-06-11 21:04:08 +0000

    Update

--- deluca/core.py
+++ deluca/core.py
@@ -24,46 +24,170 @@ arg?)
 import os
 import pickle
 import typing
+import inspect
 import jax
 import attr
 
 import deluca
-import zope.interface
-from zope.interface.verify import verifyClass
+
+import abc
+from abc import abstractmethod
 
 
-class IObj(zope.interface.Interface):
-    """Interface describing deluca objects"""
+def flatten(obj):
+    data_fields = []
+    meta_fields = []
+
+    for field in attr.fields(obj.__class__):
+        if "pytree" in field.metadata and field.metadata["pytree"]:
+            data_fields.append(field.name)
+        else:
+            meta_fields.append(field.name)
+
+    children = tuple(getattr(obj, name) for name in data_fields)
+    meta = dict(zip(meta_fields, tuple(getattr(obj, name) for name in meta_fields)))
+
+    return children, {"class": obj.__class__, "meta": meta, "data_fields": data_fields}
 
 
-class IEnv(zope.interface.Interface):
-    """Interface describing deluca environments"""
-
-    def reset():
-        """Resets the environment"""
-
-    def dynamics(action):
-        """Describes the environment dynamics and returns a new environment"""
-
-    def cost(action):
-        """Computes the cost of an action"""
+def unflatten(aux, children):
+    data_fields = aux["data_fields"]
+    data = dict(zip(data_fields, children))
+    data.update(aux["meta"])
+    return aux["class"](**data)
 
 
-class IAgent(zope.interface.Interface):
-    """Interface describing deluca agents"""
+class Obj(abc.ABC):
+    def __init_subclass__(cls, *args, **kwargs):
+        parent_cls = cls.__mro__[1]
+        for member in dir(parent_cls):
+            if isinstance(
+                inspect.getattr_static(parent_cls, member), staticmethod
+            ) and not isinstance(inspect.getattr_static(cls, member), staticmethod):
+                raise TypeError(
+                    f"Class {cls.__name__} should have staticmethod "
+                    "{member}, but {member} is not static."
+                )
 
-    def reset():
-        """Resets the agent"""
+            if hasattr(parent_cls, "__abstractmethods__") and member in getattr(
+                parent_cls, "__abstractmethods__"
+            ):
+                if member not in cls.__dict__:
+                    raise TypeError(
+                        f"Abstract method {member} not defined in class {cls.__name__}."
+                    )
 
-    def action(observation):
-        """Returns the action given an observation"""
+            if (
+                member in cls.__dict__
+                and callable(getattr(cls, member))
+                and not (
+                    inspect.signature(getattr(cls, member))
+                    == inspect.signature(getattr(parent_cls, member))
+                )
+            ):
+                raise TypeError(
+                    f"Method `{member}` should have the same signature in class `{cls.__name__}` as in its parent class `{parent_cls.__name__}`"
+                )
 
-    def update(reward):
-        """Updates the agent with the reward"""
+        if not hasattr(cls, "__annotations__"):
+            setattr(cls, "__annotations__", {})
+
+        for member in set(dir(cls)) - set(dir(parent_cls)):
+            if (
+                member.startswith("_")
+                or callable(getattr(cls, member))
+                or (
+                    member in cls.__annotations__
+                    and hasattr(cls.__annotations__[member], "__origin__")
+                    and cls.__annotations__[member].__origin__ is typing.ClassVar
+                )
+            ):
+                continue
+
+            value = getattr(cls, member)
+
+            if not isinstance(getattr(cls, member), attr._make._CountingAttr):
+                setattr(cls, member, attr.ib(default=value))
+
+            if member not in cls.__annotations__:
+                cls.__annotations__[member] = type(value)
+
+        attr.s(cls, frozen=True, kw_only=True)
+        jax.tree_util.register_pytree_node(cls, flatten, unflatten)
+
+    @staticmethod
+    def evolve(obj, **changes):
+        return attr.evolve(obj, **changes)
+
+    def save(self, path):
+        dirname = os.path.abspath(os.path.dirname(path))
+        if not os.path.exists(dirname):
+            os.makedirs(dirname)
+
+        with open(path, "wb") as file:
+            pickle.dump(self, file)
+
+    @classmethod
+    def load(cls, path):
+        return pickle.load(open(path, "rb"))
 
 
-def evolve(obj, **changes):
-    return attr.evolve(obj, **changes)
+class Env(Obj):
+    @staticmethod
+    @abstractmethod
+    def reset(env):
+        raise NotImplementedError()
+
+    @staticmethod
+    @abstractmethod
+    def dynamics(env, action):
+        raise NotImplementedError()
+
+    @staticmethod
+    @abstractmethod
+    def cost(env, action):
+        return NotImplementedError()
+
+    @staticmethod
+    def f_x(env, action):
+        return jax.jacfwd(env.dynamics, argnums=0)(env, action)
+
+    @staticmethod
+    def f_u(env, action):
+        return jax.jacfwd(env.dynamics, argnums=1)(env, action)
+
+    @staticmethod
+    def c_x(env, action):
+        return jax.grad(env.cost, argnums=0)(env, action)
+
+    @staticmethod
+    def c_u(env, action):
+        return jax.grad(env.cost, argnums=1)(env, action)
+
+    @staticmethod
+    def c_xx(env, action):
+        return jax.hessian(env.cost, argnums=0)(env, action)
+
+    @staticmethod
+    def c_uu(env, action):
+        return jax.hessian(env.cost, argnums=1)(env, action)
+
+
+class Agent(Obj):
+    @staticmethod
+    @abstractmethod
+    def reset(agent):
+        raise NotImplementedError()
+
+    @staticmethod
+    @abstractmethod
+    def action(agent, observation):
+        raise NotImplementedError()
+
+    @staticmethod
+    @abstractmethod
+    def update(agent, observation):
+        raise NotImplementedError()
 
 
 def attrib(default=None, pytree=False, **kwargs):
@@ -82,125 +206,8 @@ def pytree(default=None, **kwargs):
     return attrib(default=default, pytree=True, **kwargs)
 
 
-def save(obj, path):
-    dirname = os.path.abspath(os.path.dirname(path))
-    if not os.path.exists(dirname):
-        os.makedirs(dirname)
-
-    with open(path, "wb") as file:
-        pickle.dump(obj, file)
-
-
-def load(path):
-    return pickle.load(open(path, "rb"))
-
-
-def flatten(self):
-    data_fields = []
-    meta_fields = []
-
-    for field in attr.fields(self.__class__):
-        if "pytree" in field.metadata and field.metadata["pytree"]:
-            data_fields.append(field.name)
-        else:
-            meta_fields.append(field.name)
-
-    children = tuple(getattr(self, name) for name in data_fields)
-    meta = dict(zip(meta_fields, tuple(getattr(self, name) for name in meta_fields)))
-
-    return children, {"class": self.__class__, "meta": meta, "data_fields": data_fields}
-
-
-def unflatten(aux, children):
-    data_fields = aux["data_fields"]
-    data = dict(zip(data_fields, children))
-    data.update(aux["meta"])
-    return aux["class"](**data)
-
-
-def _make_and_check_class(iface, cls, statics=None):
-    statics = statics or []
-
-    for member in set(dir(cls)) - set(statics):
-        if member.startswith("_") or (
-            member in cls.__annotations__
-            and hasattr(cls.__annotations__[member], "__origin__")
-            and cls.__annotations__[member].__origin__ is typing.ClassVar
-        ):
-            continue
-
-        value = getattr(cls, member)
-        if not isinstance(getattr(cls, member), attr._make._CountingAttr):
-            setattr(cls, member, attr.ib(default=value))
-        cls.__annotations__[member] = type(value)
-
-    # NOTE: We avoid setting functions without inheritance, but make an
-    # exception here
-    for member in statics:
-        if not isinstance(cls.__dict__[member], staticmethod):
-            raise TypeError(
-                f"All public methods must be static. Method "
-                f"`{member}` in `{cls.__name__}` is not. Either make it "
-                f"static or prefix with `_` to indicate it is "
-                f"private."
-            )
-
-    cls = attr.s(auto_attribs=True, kw_only=True)(cls)
-
-    cls = zope.interface.implementer(iface)(cls)
-    verifyClass(iface, cls)
-
-    jax.tree_util.register_pytree_node(cls, flatten, unflatten)
-
-    return cls
-
-
-def obj(cls: type):
-    return _make_and_check_class(IObj, cls, [])
-
-
-def env(cls: type):
-    # NOTE: And adding a bunch of functions here...
-    def f_x(env, action):
-        return jax.jacfwd(cls.dynamics, argnums=0)(env, action)
-
-    def f_u(env, action):
-        return jax.jacfwd(cls.dynamics, argnums=1)(env, action)
-
-    def c_x(env, action):
-        return jax.grad(cls.cost, argnums=0)(env, action)
-
-    def c_u(env, action):
-        return jax.grad(cls.cost, argnums=1)(env, action)
-
-    def c_xx(env, action):
-        return jax.hessian(cls.cost, argnums=0)(env, action)
-
-    def c_uu(env, action):
-        return jax.hessian(cls.cost, argnums=1)(env, action)
-
-    for func in [f_x, f_u, c_x, c_u, c_xx, c_uu]:
-        if not hasattr(cls, func.__name__):
-            setattr(cls, func.__name__, staticmethod(func))
-
-    cls = _make_and_check_class(
-        IEnv, cls, ["reset", "dynamics", "cost", "f_x", "f_u", "c_x", "c_u", "c_xx", "c_uu"]
-    )
-
-    return cls
-
-
-def agent(cls: type):
-    return _make_and_check_class(IAgent, cls, ["reset", "action", "update"])
-
-
-deluca.obj = obj
-deluca.env = env
-deluca.agent = agent
-deluca.evolve = evolve
+deluca.Obj = Obj
+deluca.Env = Env
+deluca.Agent = Agent
 deluca.attrib = attrib
 deluca.pytree = pytree
-deluca.save = save
-deluca.load = load
-deluca.IEnv = IEnv
-deluca.IAgent = IAgent
