f25d63154af4d93f4ea4f6bd0c46ae0196d854de
Daniel Suo <danielsuo@gmail.com>
2021-06-17 16:49:42 +0000

    Update core

--- deluca/core.py
+++ deluca/core.py
@@ -28,70 +28,13 @@ import os
 import pickle
 import inspect
 import jax
-from flax.struct import dataclass
-
-import deluca
 
 from abc import abstractmethod
 
+import flax
+from flax.struct import dataclass
 
-def attr_list_to_dict(obj, attrs):
-    return {name: getattr(obj, name) for name in attrs}
-
-
-def flatten(obj):
-    meta = [name for name in dir(obj) if not name.startswith("__")]
-    if hasattr(obj, "jax_attrs"):
-        data = obj.jax_attrs()
-    else:
-        data = [name for name in meta if not callable(getattr(obj, name))]
-
-    meta = list(set(meta) - set(data))
-
-    children, treedef = jax.tree_util.tree_flatten(attr_list_to_dict(obj, data))
-
-    aux = {
-        "class": obj.__class__,
-        "treedef": treedef,
-        "args": obj.__args__,
-        "meta": attr_list_to_dict(obj, meta),
-    }
-
-    return children, aux
-
-
-def unflatten(aux, children):
-    obj = aux["class"](*aux["args"].args, **aux["args"].kwargs)
-    data = jax.tree_util.tree_unflatten(aux["treedef"], children)
-    for key, val in data.items():
-        object.__setattr__(obj, key, val)
-    for key, val in aux["meta"]:
-        object.__setattr__(obj, key, val)
-    return obj
-
-
-def register_pytree_node(cls):
-    jax.tree_util.register_pytree_node(cls, flatten, unflatten)
-
-    def __new__(cls, *args, **kwargs):
-        obj = object.__new__(cls)
-        object.__setattr__(obj, "__args__", inspect.signature(obj.__init__).bind(*args, **kwargs))
-        object.__getattribute__(obj, "__args__").apply_defaults()
-        return obj
-
-    cls.__new__ = __new__
-
-    return cls
-
-
-def dataclass(cls):
-    cls = register_pytree_node(cls)
-    cls = dataclasses.dataclass(frozen=True)(cls)
-    return cls
-
-
-def evolve(obj, **changes):
-    return attr.evolve(obj, **changes)
+import deluca
 
 
 def save(obj, path):
@@ -107,54 +50,47 @@ def load(path):
     return pickle.load(open(path, "rb"))
 
 
-class Obj():
-    def __attrs_post_init__(self):
+def field(default=None, **kwargs):
+    return flax.struct.field(default=default, **kwargs)
+
+
+class Obj:
+    def __new__(cls, *args, **kwargs):
+        """A true bastardization of __new__..."""
+        old_setattr = cls.__setattr__
+        cls.__setattr__ = lambda self, name, value: object.__setattr__(self, name, value)
+        obj = object.__new__(cls)
+        obj.setup()
+        cls.__setattr__ = old_setattr
+        return obj
+
+    @classmethod
+    def __init_subclass__(cls, *args, **kwargs):
+        dataclass(cls)
+
+    @abstractmethod
+    def __call__(self):
+        """Return an updated state"""
+
+    def setup(self):
+        """Used in place of __init__"""
         pass
 
 
 class Env(Obj):
-    def __call__(self, state, action):
-        raise NotImplementedError()
-
-    def f_x(self, state, action):
-        return jax.jacfwd(state.dynamics, argnums=0)(state, action)
-
-    def f_u(self, state, action):
-        return jax.jacfwd(state.dynamics, argnums=1)(state, action)
-
-    def c_x(self, state, action):
-        return jax.grad(state.cost, argnums=0)(state, action)
-
-    def c_u(self, state, action):
-        return jax.grad(state.cost, argnums=1)(state, action)
-
-    def c_xx(self, state, action):
-        return jax.hessian(state.cost, argnums=0)(state, action)
-
-    def c_uu(self, state, action):
-        return jax.hessian(state.cost, argnums=1)(state, action)
+    @abstractmethod
+    def init(self):
+        """Return an initialized state"""
 
 
 class Agent(Obj):
-    @staticmethod
-    @abstractmethod
-    def reset(agent):
-        raise NotImplementedError()
-
-    @staticmethod
-    @abstractmethod
-    def action(agent, observation):
-        raise NotImplementedError()
-
-    @staticmethod
-    @abstractmethod
-    def update(agent, observation):
-        raise NotImplementedError()
+    pass
 
 
+deluca.dataclass = dataclass
+deluca.field = field
 deluca.Obj = Obj
 deluca.Env = Env
 deluca.Agent = Agent
-deluca.evolve = evolve
 deluca.save = save
 deluca.load = load
