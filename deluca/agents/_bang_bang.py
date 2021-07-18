484fda50ec6223817603f8bdf703621e625a1d99
Daniel Suo <danielsuo@gmail.com>
2021-07-18 12:03:20 -0400

    Update

--- deluca/agents/_bang_bang.py
+++ deluca/agents/_bang_bang.py
@@ -11,10 +11,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-from typing import Callable
-
-import jax.numpy as jnp
-
 from deluca.core import Agent
 from deluca.core import field
 
