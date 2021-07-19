2dcf2dce94527887e122d0fd282945446503033a
Daniel Suo <danielsuo@gmail.com>
2021-07-19 18:19:52 +0000

    Update

--- deluca/agents/_random.py
+++ deluca/agents/_random.py
@@ -11,9 +11,22 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-from deluca.agents._bang_bang import BangBang
-from deluca.agents._pid import PID
-from deluca.agents._random import Random
-from deluca.agents._zero import Zero
+from typing import Callable
 
-__all__ = ["BangBang", "PID", "Random", "Zero"]
+import jax
+
+from deluca.core import Agent
+from deluca.core import field
+
+
+class Random(Agent):
+    seed: int = field(0, jaxed=False)
+    func: Callable = field(lambda key: 0.0, jaxed=False)
+
+    def init(self):
+        return jax.random.PRNGKey(self.seed)
+
+    def __call__(self, state, obs):
+        key, subkey = jax.random.split(state)
+
+        return subkey, self.func(key)
