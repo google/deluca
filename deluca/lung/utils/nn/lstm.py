e5a31a751cb9449dc0c633d3fc937036de18400e
Daniel Suo <dsuo@google.com>
2025-06-09 13:24:27 +0000

    Flax is changing the `RNNCellBase` API: - when calling the constructor of a class, it is now required to pass in a `features` argument - when calling the `initialize_carry` method, instead of passing in the `batch_dims` and `size`, you only have to pass in an `input_shape`
    More details about the changes and how to upgrade to the new API can be found [here](https://flax--3053.org.readthedocs.build/en/3053/guides/rnncell_upgrade_guide.html).

--- deluca/lung/utils/nn/lstm.py
+++ deluca/lung/utils/nn/lstm.py
@@ -15,57 +15,63 @@
 """lstm module."""
 import functools
 from typing import Callable
+
 import flax.linen as nn
 import jax
 import jax.numpy as jnp
-
 
 # pylint: disable=g-bare-generic
 
 
 class LSTM(nn.Module):
-  """LSTM encoder, returning state after EOS is input."""
-  n_layers: int = 2  # or 4
-  hidden_dim: int = 20
-  out_dim: int = 1
-  bptt: int = 3  # or 10 or 29
-  activation_fn: Callable = nn.tanh
+    """LSTM encoder, returning state after EOS is input."""
 
-  @nn.compact
-  def __call__(self, inputs):
-    # inputs.shape = (batch_size, seq_length, vocab_size).
-    batch_size = (inputs.shape[0],)
-    lstm = LSTMNetwork(
-        n_layers=self.n_layers, activation_fn=self.activation_fn)
-    init_lstm_state = lstm.initialize_carry(batch_size, self.hidden_dim)
-    init_lstm_state = (init_lstm_state[0].astype(jnp.float64),
-                       init_lstm_state[1].astype(jnp.float64))
-    _, y = lstm(init_lstm_state, inputs)
-    y = nn.Dense(features=self.out_dim, use_bias=True, name='lstm_fc')(y)
-    return y[0, -1, 0]  # squeeze for consistent shape w/ boundary model output
+    n_layers: int = 2  # or 4
+    hidden_dim: int = 20
+    out_dim: int = 1
+    bptt: int = 3  # or 10 or 29
+    activation_fn: Callable = nn.tanh
+
+    @nn.compact
+    def __call__(self, inputs):
+        # inputs.shape = (batch_size, seq_length, vocab_size).
+        batch_size = (inputs.shape[0],)
+        lstm = LSTMNetwork(n_layers=self.n_layers, activation_fn=self.activation_fn)
+        init_lstm_state = lstm.initialize_carry(batch_size, self.hidden_dim)
+        init_lstm_state = (
+            init_lstm_state[0].astype(jnp.float64),
+            init_lstm_state[1].astype(jnp.float64),
+        )
+        _, y = lstm(init_lstm_state, inputs)
+        y = nn.Dense(features=self.out_dim, use_bias=True, name="lstm_fc")(y)
+        return y[0, -1, 0]  # squeeze for consistent shape w/ boundary model output
 
 
 class LSTMNetwork(nn.Module):
-  """A simple unidirectional LSTM."""
-  n_layers: int = 2  # or 4
-  activation_fn: Callable = nn.tanh
+    """A simple unidirectional LSTM."""
 
-  @functools.partial(
-      nn.transforms.scan,
-      variable_broadcast='params',
-      in_axes=1,
-      out_axes=1,
-      split_rngs={'params': False})
-  @nn.compact
-  def __call__(self, carry, x):
-    for _ in range(self.n_layers):
-      carry, x = nn.OptimizedLSTMCell(activation_fn=self.activation_fn)(carry,
-                                                                        x)
-    return carry, x
+    n_layers: int = 2  # or 4
+    activation_fn: Callable = nn.tanh
 
-  @staticmethod
-  def initialize_carry(batch_dims, hidden_dim):
-    # (N, C, H, W) = (N, 1, 1, W) where W = feature_dim and N is batch_size
-    # Use fixed random key since default state init fn is just zeros.
-    return nn.OptimizedLSTMCell.initialize_carry(
-        jax.random.PRNGKey(0), batch_dims, hidden_dim)
+    @functools.partial(
+        nn.transforms.scan,
+        variable_broadcast="params",
+        in_axes=1,
+        out_axes=1,
+        split_rngs={"params": False},
+    )
+    @nn.compact
+    def __call__(self, carry, x):
+        for _ in range(self.n_layers):
+            carry, x = nn.OptimizedLSTMCell(features, activation_fn=self.activation_fn)(
+                carry, x
+            )
+        return carry, x
+
+    @staticmethod
+    def initialize_carry(batch_dims, hidden_dim):
+        # (N, C, H, W) = (N, 1, 1, W) where W = feature_dim and N is batch_size
+        # Use fixed random key since default state init fn is just zeros.
+        return nn.OptimizedLSTMCell.(hidden_dim, parent=-None).initialize_carry(
+            jax.random.PRNGKey(0), batch_dims, hidden_dim
+        )
