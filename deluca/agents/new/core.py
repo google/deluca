Better Colabs, improved Normalization (but maybe not fully fixed)

deluca/agents/new/core.py
@@ -8,7 +8,7 @@ import optax
 
 from deluca.core import Env
 from deluca.memory import Memory, MemorySettings
-from deluca.normalizers.core import Normalizers, WithoutNormalization
+from deluca.normalizers.core import NormalizerSet, NoNormalization
 from deluca.utils.printing import Task
 from abc import abstractmethod
 import jax.numpy as jnp
@@ -17,7 +17,6 @@ import flax.nnx as nnx
 
 History = Tuple[Array, Array]
 LossFunction = Callable[[Array, Any, Array], Array | chex.Array]
-
 
 @flax.struct.dataclass
 class AgentSettings:
@@ -63,7 +62,7 @@ class Agent:
     history_length: int
     memory_settings: MemorySettings
 
-    normalizers: Normalizers
+    normalizers: NormalizerSet
     optimizer: nnx.Optimizer
 
     def __init__(
@@ -71,8 +70,15 @@ class Agent:
         memory_settings: MemorySettings,
         settings: AgentSettings,
         rng: Array,
-        normalizers: Normalizers | None = None,
+        normalizers: NormalizerSet | None = None,
     ):
+        """
+        Args:
+            memory_settings: The memory settings for the agent.
+            settings: The settings for the agent.
+            rng: A JAX PRNG key
+            normalizers: The normalizers for the agent. Note: normalization parameters must be computed manually, unlike learners, since agents are never exposed to a large batch of data with which to compute normalization parameters.
+        """
         self.obs_dim_in = memory_settings.obs_dim_in
         self.action_dim_in = memory_settings.action_dim_in
         self.obs_dim_out = memory_settings.obs_dim_out
@@ -81,7 +87,7 @@ class Agent:
 
         self.settings = settings
 
-        self.normalizers = normalizers or WithoutNormalization()
+        self.normalizers = normalizers or NoNormalization()
 
         self.memory_settings = memory_settings
 
@@ -208,8 +214,8 @@ class Agent:
             action_history.shape[hist_index + 1] == self.action_dim_in
         ), "actions must have action_dim_in dimensions"
 
-        obs_history = self.normalizers.normalize_obs(obs_history)
-        action_history = self.normalizers.normalize_action(action_history)
+        obs_history = self.normalizers.obs.normalize(obs_history)
+        action_history = self.normalizers.action.normalize(action_history)
 
         return obs_history, action_history
 
@@ -217,7 +223,7 @@ class Agent:
         """
         Postprocess an action for prediction.
         """
-        action = self.normalizers.denormalize_action(action)
+        action = self.normalizers.output.denormalize(action)
         action = jnp.expand_dims(action, -1)  # Add a singleton dimension to the action
 
         return action

deluca/learners/core.py
@@ -7,7 +7,7 @@ import chex
 from deluca.agents._random import SimpleRandom
 from deluca.core import Env
 from deluca.memory import MemorySettings
-from deluca.normalizers.core import Normalizers, WithoutNormalization
+from deluca.normalizers.core import NoNormalization, NormalizerSet
 from deluca.utils.printing import Task
 from abc import abstractmethod
 import jax.numpy as jnp
@@ -65,7 +65,7 @@ class Learner:
     action_dim_out: int
     history_length: int
 
-    normalizers: Normalizers
+    normalizers: NormalizerSet
     optimizer: nnx.Optimizer
     loss_fn: Callable[
         [Array, Array], Array | chex.Array
@@ -76,7 +76,7 @@ class Learner:
         memory_settings: MemorySettings,
         settings: LearnerSettings,
         rng: Array,
-        normalizers: Normalizers | None = None,
+        normalizers: NormalizerSet | None = None,
     ):
         self.obs_dim_in = memory_settings.obs_dim_in
         self.action_dim_in = memory_settings.action_dim_in
@@ -86,7 +86,7 @@ class Learner:
 
         self.settings = settings
 
-        self.normalizers = normalizers or WithoutNormalization()
+        self.normalizers = normalizers or NoNormalization()
         self.optimizer = nnx.Optimizer(
             self.model,
             self.settings.optimizer_fn,
@@ -109,7 +109,9 @@ class Learner:
 
         # For training, we need to compute the normalization of the observations and actions (e.g. mean and std)
         # These values will be stored and used for denormalization during prediction.
-        self.normalizers.compute_normalization(obs, actions)
+        self.normalizers.obs.compute(obs)
+        self.normalizers.action.compute(actions)
+        self.normalizers.output.compute(next_obs)
 
         histories = self._preprocess_histories(histories)
 
@@ -261,9 +263,9 @@ class Learner:
         ), "Next observations must have obs_dim_out dimensions"
 
         # Normalization
-        obs = self.normalizers.normalize_obs(obs)
-        actions = self.normalizers.normalize_action(actions)
-        next_obs = self.normalizers.normalize_obs(next_obs)
+        obs = self.normalizers.obs.normalize(obs)
+        actions = self.normalizers.action.normalize(actions)
+        next_obs = self.normalizers.output.normalize(next_obs)
 
         return obs, actions, next_obs
 
@@ -298,8 +300,8 @@ class Learner:
             action_history.shape[hist_index + 1] == self.action_dim_in
         ), "actions must have action_dim_in dimensions"
 
-        obs_history = self.normalizers.normalize_obs(obs_history)
-        action_history = self.normalizers.normalize_action(action_history)
+        obs_history = self.normalizers.obs.normalize(obs_history)
+        action_history = self.normalizers.action.normalize(action_history)
 
         return obs_history, action_history
 
@@ -308,7 +310,7 @@ class Learner:
         Postprocess the prediction (e.g. denormalize and add back singleton dimension)
         """
 
-        pred_obs = self.normalizers.denormalize_obs(pred_obs)
+        pred_obs = self.normalizers.output.denormalize(pred_obs)
 
         return jnp.expand_dims(pred_obs, -1)
 

deluca/normalizers/core.py
@@ -1,82 +1,59 @@
 from abc import abstractmethod
 from jax import Array
 import jax.numpy as jnp
+import flax.struct
 
-
-class Normalizers:
+# TODO: Fix normalization.
+class Normalizer:
     @abstractmethod
-    def normalize_obs(
-        self, obs: Array
-    ) -> Array:
-        """Normalize the input observation Array."""
-
-    @abstractmethod
-    def normalize_action(
-        self, action: Array
-    ) -> Array:
-        """Normalize the input action Array."""
+    def compute(self, a: Array) -> None:
+        """Computes the necessary parameters from the input to normalize and denormalize future inputs."""
 
     @abstractmethod
-    def denormalize_obs(
-        self, obs: Array
-    ) -> Array:
-        """Denormalize the input observation Array."""
+    def normalize(self, a: Array) -> Array:
+        """Normalizes the input."""
 
     @abstractmethod
-    def denormalize_action(
-        self, action: Array
-    ) -> Array:
-        """Denormalize the input action Array."""
+    def denormalize(self, a: Array) -> Array:
+        """Denormalizes the input."""
 
-    @abstractmethod
-    def compute_normalization(self, obses: Array, actions: Array) -> None:
-        """Compute the normalization of the input."""
+@flax.struct.dataclass
+class NormalizerSet:
+    obs: Normalizer
+    action: Normalizer
+    output: Normalizer
 
-
-class WithoutNormalization(Normalizers):
-    def normalize_obs(self, obs: Array) -> Array:
-        return obs
-
-    def normalize_action(self, action: Array) -> Array:
-        return action
-
-    def denormalize_obs(self, obs: Array) -> Array:
-        return obs
-
-    def denormalize_action(self, action: Array) -> Array:
-        return action
-
-    def compute_normalization(self, obses, actions):
+class WithoutNormalization(Normalizer):
+    def compute(self, a: Array) -> None:
         pass
 
+    def normalize(self, a: Array) -> Array:
+        return a
+    
+    def denormalize(self, a: Array) -> Array:
+        return a
+    
+class DefaultNormalizer(Normalizer):
+    def compute(self, a: Array) -> None:
+        flat_a = a.reshape(-1, a.shape[-1])
+        self.mean = jnp.mean(flat_a, axis=0) 
+        self.std = jnp.std(flat_a, axis=0)
+    
+    def normalize(self, a: Array) -> Array:
+        return (a - self.mean) / (self.std + 1e-8)
+    
+    def denormalize(self, a: Array) -> Array:
+        return a * (self.std + 1e-8) + self.mean
 
-class DefaultNormalizers(Normalizers):
-    _eps: float = 1e-8
+NoNormalization = lambda: NormalizerSet(
+    obs=WithoutNormalization(),
+    action=WithoutNormalization(),
+    output=WithoutNormalization(),
+)
 
-    def normalize_obs(
-        self,
-        obs: Array,
-    ) -> Array:
-        return (obs - self.obs_mean) / (self.obs_std + self._eps)
+DefaultNormalizers = lambda: NormalizerSet(
+    obs=DefaultNormalizer(),
+    action=DefaultNormalizer(),
+    output=DefaultNormalizer(),
+)
 
-    def normalize_action(
-        self, action: Array
-    ) -> Array:
-        return (action - self.action_mean) / (self.action_std + self._eps)
-
-    def denormalize_obs(
-        self,
-        obs: Array,
-    ) -> Array:
-        return obs * (self.obs_std + self._eps) + self.obs_mean
-
-    def denormalize_action(
-        self, action: Array
-    ) -> Array:
-        return action * (self.action_std + self._eps) + self.action_mean
-
-    def compute_normalization(self, obses: Array, actions: Array) -> None:
-        self.obs_mean = jnp.mean(obses, axis=0)
-        self.obs_std = jnp.sqrt(jnp.var(obses, axis=0))
-        self.action_mean = jnp.mean(actions, axis=0)
-        self.action_std = jnp.sqrt(jnp.var(actions, axis=0))